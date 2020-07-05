06310e0ab5d93de2a9c059c2d8067181759c11af
José Bezerra <josbezfi@gmail.com>
2020-07-05 10:16:34 -0300

    Iniciando curso Java - Prof. Nélio Alves(Udemy)

---
+++ curso_programacao/src/Trapezio.java
@@ -0,0 +1,24 @@
+
+public class Trapezio {
+
+	public static void main(String[] args) {
+		
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		
+		area = (b + B) / 2.0 * h;
+		System.out.println(area);
+		
+		float base, Base, altura, areaFloat;
+		base = 6f;
+		Base = 8f;
+		altura = 5f;
+		
+		areaFloat = (base + Base) / 2f * altura;
+		System.out.println(areaFloat);
+		
+	}
+
+}
